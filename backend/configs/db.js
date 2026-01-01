ddc1966c2e586b44d63207beb4bc968569ec09d2
ice1425 <icelove142548@gmail.com>
2026-01-02 02:02:50 +0700

    edit connectDB

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,23 +1,27 @@
 const e = require("express");
 const mysql = require("mysql2");
 
-const dbconn = mysql.createConnection({
+const dbconn = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+      connectionLimit: 10, 
+      queueLimit: 0,
   ssl: {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: false
     }
 });
 
-dbconn.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log("connect successed");
-})
+
+// dbconn.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log("connect successed");
+// })
 
 module.exports = dbconn;
