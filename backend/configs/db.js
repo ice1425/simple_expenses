068cd7f0cf359d5f727f064a53c9a9a0ad0df5c5
ice1425 <icelove142548@gmail.com>
2026-01-02 00:32:46 +0700

    add ssl

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -5,6 +5,11 @@ const dbconn = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB,
+  port: process.env.DB_PORT ,
+  ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: false
+    }
 });
 
 dbconn.connect((err) => {
